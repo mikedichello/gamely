d040c78fb37096264ac97dae7d09c1f33209f355
mikedichello <michael.dichello@gmail.com>
2019-11-27 16:48:17 -0600

    heroku work please?

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ `games`;
 
-mongoose.connect('mongodb://localhost:27017/games', {
+mongoose.connect(MONGODB_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: true
 });
+
 mongoose.Promise = Promise;
 
 module.exports = mongoose;
